11b09bba8d2930b86f60b9958172a431749ac6fa
github 0322keisuke <isikeidesk@gmail.com>
2020-05-28 20:37:07 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const cards = ['1','1','2','2','3','3','4','4','5','5','6','6','7','7','8','8','9','10'];
+  const [deck, setDeck] = useState(cards);
+  const [hand, setHand] = useState(0);
+  // const lastCard = deck[deck.length-1];
+
+  const ShuffleDeck = () => {
+    const newCards = shuffle(deck);
+    const lastCard = newCards[newCards.length-1];
+    
+    setDeck(newCards);
+
+    return lastCard;
+  }
+
+  const shuffle = ([...array]) => {
+    for (let i = array.length - 1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  } 
+
+  const DrawFromDeck = () => {
+    const lastCard = ShuffleDeck();
+    // setDeck(deck.pop());
+    console.log("deck=",deck) ;
+    console.log("lastCard=",lastCard);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div>カード表示２</div>
+      <div>デッキ：{deck[deck.length-1]}</div> 
+      <div>手札：{hand}</div>
+      <div>
+        <button onClick={ShuffleDeck}>デッキをシャッフル</button>
+        <button onClick={DrawFromDeck}>デッキから１枚引く</button> 
+      </div>
+    </>
+  )
 }
 
 export default App;
